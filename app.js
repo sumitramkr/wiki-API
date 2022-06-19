a20dcc13887e5e0cc48ac627d7aef7bbdc9f9b8d
sumitramkr <sumitramkr77e@gmail.com>
2022-06-20 00:16:47 +0530

    connected mongoose

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParse: true });
+
+app.listen(3400, function () {
+  console.log("Server started on port 3400");
+});
